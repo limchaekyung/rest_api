e50ca77e83626b345e14090281e238b1b99e0e88
limchaekyung <corud505@naver.com>
2022-04-30 23:48:00 +0900

    get

---
+++ routes/subscribers.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+const Subscriber = require('../models/subscriber')
+
+// Getting all
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// Getting One
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+// Creating One
+router.post('/', (req, res) => {
+
+})
+
+// Updating One
+router.patch('/', (req, res) => {
+
+})
+
+// Deleting One
+router.delete('/', (req, res) => {
+
+})
+
+module.exports = router
